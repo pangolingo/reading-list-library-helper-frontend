16b2505d632489b0aadc1ce4fac1512d06551a80
David Iverson <diverson@DIVERSON.local>
2017-08-25 10:09:45 -0500

    configured local, production API urls

--- src/App.js
+++ src/App.js
@@ -7,7 +7,12 @@ import ShelfPicker from './ShelfPicker.js'
 import GoodreadsService from './GoodreadsService.js'
 import localForage from 'localforage';
 
-const apiBaseUrl = "http://localhost:4567";
+let apiBaseUrl;
+if(process.env.NODE_ENV === "production"){
+  apiBaseUrl = "https://library-helper-api.herokuapp.com";
+} else {
+  apiBaseUrl = "http://localhost:4567";
+}
 const oauthUrl = `${apiBaseUrl}/oauth?callback_url=${encodeURIComponent(window.location.href)}`;
 
 const fakeAuth = {
